158d42525945c34b2f0d632e96b3b8cd0c600bc9
werbower <werbower@gmail.com>
2018-05-18 12:03:36 +0300

    Add project files.

---
+++ BusinessWorkflow/MyWF.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace BusinessWorkflow {
+    class MyWF : CodeActivity {
+        protected override void Execute(CodeActivityContext context) {
+            throw new NotImplementedException();
+        }
+    }
+}
